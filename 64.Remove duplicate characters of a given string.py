79a715bbf4060f2771254544783540254bae1dbc
Abhijeet Anand <60473082+Abhijeet198@users.noreply.github.com>
2020-04-03 00:11:01 +0530

    Add files via upload

---
+++ 64.Remove duplicate characters of a given string.py
@@ -0,0 +1,9 @@
+#65.Remove duplicate characters of a given string
+
+
+from collections import OrderedDict
+def remove_duplicate(str1):
+  return "".join(OrderedDict.fromkeys(str1))
+     
+print(remove_duplicate("python exercises practice solution"))
+print(remove_duplicate("w3resource"))
