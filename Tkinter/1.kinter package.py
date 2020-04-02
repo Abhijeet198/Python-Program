d8ff6b70bfa1192e12eb8fc72ad15d1a2f69706d
Abhijeet198 <abhi135anand@gmail.com>
2020-04-02 23:38:40 +0530

    Tkinter

---
+++ Tkinter/1.kinter package.py
@@ -0,0 +1,14 @@
+# WAP to import Tkinter package and create
+# a window andset its as title
+
+import tkinter as tk
+
+# Create instance
+parent = tk.TK()
+
+#Add a Title
+parent.title("-Welcome to python tkinter Bsic exercise-")
+
+#Start GUI
+parent.mainloop()
+
